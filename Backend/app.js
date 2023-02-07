f19690b2a3cd7119f23894bb35a6ebf3d0e51092
Shivam Garg <shivamgarg1324@gmail.com>
2023-02-07 17:31:52 +0530

    db changes

--- Backend/app.js
+++ Backend/app.js
@@ -1,19 +1,26 @@
 const express = require('express');
 const bodyParser = require("body-parser");
 const path = require('path');
-const InitiateMongoServer = require("./config/db");
 const user = require("./routes/user");
 const admin=require("./routes/admin")
 const cors = require('cors');
+const mongoose = require("mongoose");
 const session=require('express-session')
 const errorController = require('./controllers/error');
 const dotenv = require('dotenv');
 dotenv.config();
-InitiateMongoServer();
+
+const http = require('http');
+const socketio = require('socket.io');
+
 const app = express();
+const server = http.createServer(app)
+const io = socketio(server, { cors: { origin: '*' } })
+
 const PORT = process.env.PORT || 3000;
 const cookieParser = require("cookie-parser");
 const firebase = require('./firebase').initialize()
+
 app.use(express.json())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -33,7 +40,24 @@ app.use(user);
 app.use('/admin',admin);
 app.use(errorController.get404);
 
+
+
 app.listen(PORT, (req, res) => {
     console.log(`Server Started at PORT ${PORT}`);
 });
-  
+
+
+const MONGOURI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@flynovate-website.bqwpz.mongodb.net/database?retryWrites=true&w=majority`;
+mongoose.set("strictQuery", false);
+mongoose
+    .connect(MONGOURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then((result) => {
+      console.log("Db Connected")
+    })
+    .catch(err => {
+        console.log('Error')
+    });
+const db=mongoose.connection;
--- Backend/config/db.js
+++ Backend/config/db.js
@@ -6,17 +6,20 @@ dotenv.config();
 const MONGOURI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@flynovate-website.bqwpz.mongodb.net/database?retryWrites=true&w=majority`;
 
 mongoose.set("strictQuery", false);
-const InitiateMongoServer = async () => {
-  try {
-    await mongoose.connect(MONGOURI, {
+
+const InitiateMongoServer = () => {
+    mongoose.connect(MONGOURI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-    });
-    console.log("Connected to DB !!");
-  } catch (e) {
+    })
+    .then(()=>
+    {
+        console.log("Connected to DB !!");
+    })
+    .catch (e=> {
     console.log(e);
     throw e;
-  }
+  });
 };
 
 module.exports = InitiateMongoServer;
