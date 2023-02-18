235eb43d1816455ece99d4921b920a960e8a85e8
Jasleen8801 <jkaur1_be21@thapar.edu>
2023-02-18 13:14:44 +0530

    login and register page

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,6 +1,6 @@
-import React,{ useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
-import axios from "axios"
+import axios from "axios";
 import { Container, Row, Col } from "react-bootstrap";
 
 import ErrorList from "../components/errorlist";
@@ -9,47 +9,50 @@ import Sidebar from "../components/sidebar";
 import Topbar from "../components/topbar";
 
 const Dashboard = () => {
-  const [user,setUser] = useState([]);
-  const [drones,setDrones]=useState([]); 
+  const [user, setUser] = useState([]);
+  const [drones, setDrones] = useState([]);
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const location = useLocation();
-  const cookieValue=location.state.cookieValue;
-  
-  
+  const cookieValue = location.state.cookieValue;
+
   useEffect(() => {
     const fetchData = async () => {
       setIsLoading(true);
       try {
-        const response = await axios.get('http://localhost:3000/home',{params:{cookieValue:cookieValue}});
-        let c=1;
-        let col1=["red","blue","green","orange","pink"];
-        let col2=["yellow-dot","blue-dot","green-dot","ltblue-dot","orange-dot"];
-        let col3=["red","blue","green","orange","pink"];
-        let col4=["yellow","blue","green","ltblue","orange"];
-        for(let i=0;i<response.data.drones.length;i++)
-        {
-          if(response.data.drones[i].type=="inspection")
-          {
-            response.data.drones[i].name="Drone "+c;
-            response.data.drones[i].colormap=col1[c-1];
-            response.data.drones[i].color=col3[c-1];
+        const response = await axios.get("http://localhost:3000/home", {
+          params: { cookieValue: cookieValue },
+        });
+        let c = 1;
+        let col1 = ["red", "blue", "green", "orange", "pink"];
+        let col2 = [
+          "yellow-dot",
+          "blue-dot",
+          "green-dot",
+          "ltblue-dot",
+          "orange-dot",
+        ];
+        let col3 = ["red", "blue", "green", "orange", "pink"];
+        let col4 = ["yellow", "blue", "green", "ltblue", "orange"];
+        for (let i = 0; i < response.data.drones.length; i++) {
+          if (response.data.drones[i].type == "inspection") {
+            response.data.drones[i].name = "Drone " + c;
+            response.data.drones[i].colormap = col1[c - 1];
+            response.data.drones[i].color = col3[c - 1];
             c++;
           }
         }
-        c=1;
-        for(let i=0;i<response.data.drones.length;i++)
-        {
-          if(response.data.drones[i].type=="cleaning")
-          {
-            response.data.drones[i].name="Drone "+c;
-            response.data.drones[i].colormap=col2[c-1];
-            response.data.drones[i].color=col4[c-1];
+        c = 1;
+        for (let i = 0; i < response.data.drones.length; i++) {
+          if (response.data.drones[i].type == "cleaning") {
+            response.data.drones[i].name = "Drone " + c;
+            response.data.drones[i].colormap = col2[c - 1];
+            response.data.drones[i].color = col4[c - 1];
             c++;
           }
         }
-        console.log(response.data.drones)
-        setUser(response.data.user)
+        console.log(response.data.drones);
+        setUser(response.data.user);
         setDrones(response.data.drones);
         setIsLoading(false);
       } catch (err) {
@@ -58,9 +61,7 @@ const Dashboard = () => {
       }
     };
     fetchData();
-  }, [])
-
-  
+  }, []);
 
   return drones.length ? (
     <div>
@@ -74,7 +75,7 @@ const Dashboard = () => {
           </Col>
           <Col md={5}>
             {/* <LiveMap drones={ drones }/> */}
-            <Maap1 drones={drones}/>
+            <Maap1 drones={drones} />
           </Col>
           <Col md={4}>
             <ErrorList drones={drones} />
@@ -88,7 +89,7 @@ const Dashboard = () => {
         <Topbar user={user} />
       </Row>
       <Row className="d-flex justify-content-center">
-        <h4 style={{color: "#2a265f"}}>Flynovate</h4>
+        <h4 style={{ color: "#2a265f" }}>Flynovate</h4>
         <p className="text-dark">
           Please contact FLYNOVATE team for your DRONE requirements.
         </p>
--- frontend/src/pages/userlogin.js
+++ frontend/src/pages/userlogin.js
@@ -1,7 +1,10 @@
-import React, {useState,useEffect}  from "react";
+import React, { useState } from "react";
 import "../styles/Auth/Login.css";
-import axios from "axios"
-import {  useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { loadFull } from "tsparticles";
+import Particles from "react-tsparticles";
+import logo from "../assets/logo2.png";
 
 const Login = () => {
   const [error, setError] = useState("");
@@ -10,49 +13,178 @@ const Login = () => {
   const navigate = useNavigate();
 
   const handleChange = ({ currentTarget: input }) => {
-		setuser({ ...user, [input.name]: input.value });
-	};
+    setuser({ ...user, [input.name]: input.value });
+  };
   const handleSubmit = async (e) => {
-		e.preventDefault();
-		try {
-			const url = "http://localhost:3000/login";
-			const res = await axios.post(url, user);
-      const cookie=res.data.access;
+    e.preventDefault();
+    try {
+      const url = "http://localhost:3000/login";
+      const res = await axios.post(url, user);
+      const cookie = res.data.access;
       setCookieValue(cookie);
-		  navigate("/home",{state:{user:res.data.user,cookieValue:res.data.access}});
-		} catch (error) {
-			if (
-				error.response &&
-				error.response.status >= 400 &&
-				error.response.status <= 500
-			) {
-				setError(error.response.data.message);
-			}
-		}
-	};
+      navigate("/home", {
+        state: { user: res.data.user, cookieValue: res.data.access },
+      });
+    } catch (error) {
+      if (
+        error.response &&
+        error.response.status >= 400 &&
+        error.response.status <= 500
+      ) {
+        setError(error.response.data.message);
+      }
+    }
+  };
+
+  const particlesInit = async (main) => {
+    console.log(main);
+
+    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+    // starting from v2 you can add only the features you need reducing the bundle size
+    await loadFull(main);
+  };
+
   return (
-    <div className="login-box">
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
-        <div className="user-box">
-          <input type="text" name="username" required onChange={handleChange} value={user.username}/>
-          <label>Username</label>
+    <>
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        options={{
+          detectRetina: false,
+          fpsLimit: 60,
+          interactivity: {
+            detectsOn: "canvas",
+            events: {
+              onHover: {
+                enable: true,
+                mode: "bubble",
+              },
+              resize: true,
+            },
+            modes: {
+              bubble: {
+                distance: 40,
+                duration: 2,
+                opacity: 8,
+                size: 10,
+                speed: 3,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: "#ffffff",
+              animation: {
+                enable: true,
+                speed: 40,
+                sync: true,
+              },
+            },
+            lineLinked: {
+              blink: false,
+              color: "white",
+              consent: false,
+              distance: 30,
+              enable: true,
+              opacity: 0.3,
+              width: 0.5,
+            },
+            move: {
+              attract: {
+                enable: false,
+                rotate: {
+                  x: 600,
+                  y: 1200,
+                },
+              },
+              bounce: false,
+              direction: "none",
+              enable: true,
+              outMode: "bounce",
+              random: true,
+              speed: 0.5,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: false,
+                area: 2000,
+              },
+              limit: 0,
+              value: 200,
+            },
+            opacity: {
+              animation: {
+                enable: true,
+                minimumValue: 0.05,
+                speed: 2,
+                sync: false,
+              },
+              random: false,
+              value: 1,
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              animation: {
+                enable: false,
+                minimumValue: 0.1,
+                speed: 40,
+                sync: false,
+              },
+              random: true,
+              value: 1,
+            },
+          },
+        }}
+      />
+      <div className="container fluid">
+        <div className="heading d-flex align-item-center text-light p-5">
+          <img src={logo} width="50" />
+          <h3 className="fly m-2">
+            Fly<span className="novate">novate</span>
+          </h3>
         </div>
-        <div className="user-box">
-          <input type="password" name="password" required onChange={handleChange} value={user.password}/>
-          <label>Password</label>
+        <div className="login-box">
+          <h2>Login</h2>
+          <form onSubmit={handleSubmit}>
+            <div className="user-box">
+              <input
+                type="text"
+                name="username"
+                required
+                onChange={handleChange}
+                value={user.username}
+              />
+              <label>Username</label>
+            </div>
+            <div className="user-box">
+              <input
+                type="password"
+                name="password"
+                required
+                onChange={handleChange}
+                value={user.password}
+              />
+              <label>Password</label>
+            </div>
+
+            <button type="submit">Submit</button>
+          </form>
+
+          <div className="mt-5 text-light">
+            New here?
+            <span className="p-2">
+              <a href="/signup" className="signup p-2">
+                Sign Up
+              </a>
+            </span>
+          </div>
         </div>
-
-        <button type="submit">Submit</button>
-      </form>
-
-      <div className="mt-5 text-light">
-        New here?{" "}
-        <span className="p-2">
-          <a href="/signup" className="signup p-2">Sign Up</a>
-        </span>
       </div>
-    </div>
+    </>
   );
 };
 
