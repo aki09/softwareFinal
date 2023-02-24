72b7a8c54b2c8b74423c693d6ac9f47079e0a0f5
Shivam Garg <shivamgarg1324@gmail.com>
2023-02-24 14:38:42 +0530

    error listener

--- frontend/src/components/errorlist.jsx
+++ frontend/src/components/errorlist.jsx
@@ -10,15 +10,27 @@ const styles = {
 
 const ErrorList = ({ drones }) => {
   const [navbarHeight, setNavbarHeight] = useState(0);
-  const[ErrorList,serErrorList]=useState([]);
+  const[Droneno,setDroneno]=useState();
+  const[time,setTime]=useState();
+  const[error,setError]=useState([]);
 
   useEffect(() => {
+
     const socket = io("http://localhost:3000", {
       transports: ["websocket", "polling", "flashsocket"],
     });
     socket.on("errorlist", (data) => {
       const error  = data;
-      serErrorList([...ErrorList,error]);
+      for(let i=0;i<drones.length;i++)
+      {
+        if(drones[i]._id==data.error.droneId)
+        {
+          setDroneno(drones[i].droneNo);
+          const tstamp = new Date(data.error.timestamps).getSeconds();
+          setTime(tstamp);
+          setError((preverror) => [...preverror, ...error]);
+        }
+      }
     });
     const navbar = document.querySelector(".navbar");
     setNavbarHeight(navbar.offsetHeight);
@@ -32,6 +44,7 @@ const ErrorList = ({ drones }) => {
         <Card>
           <Card.Title className="ms-3 mt-3">ERROR LIST</Card.Title>
           <Card.Body>
+
           </Card.Body>
         </Card>
       </div>
