f5c5006b693a96ce57788c44198265bd39062d7a
Shivam Garg <shivamgarg1324@gmail.com>
2023-03-31 17:38:06 +0530

    pdf report

--- Backend/controllers/userinspection.js
+++ Backend/controllers/userinspection.js
@@ -63,7 +63,9 @@ exports.generatePDF = async (req, res, next) => {
       }
       defects[arrayno].total++;
     });
-    return Object.values(defects);
+  
+    const sortedDefects = Object.values(defects).sort((a, b) => a.arrayno - b.arrayno);
+    return sortedDefects;
   };
   
   const defects = getDefects(returnedData);
@@ -95,7 +97,7 @@ exports.generatePDF = async (req, res, next) => {
   const base64Image = imageData.toString('base64');
   const dataUri = `data:image/png;base64,${base64Image}`;
 
-  let htmlString= `<html><head><title>PDF REPORT</title></head><body><img src="${dataUri}" alt="Image " style="display: block; width: 100%; height: 100%;object-fit: cover;" /><div style="position: absolute;top: 40%;left: 20%;transform: translate(-50%, -50%);text-align: center;"><h3 id="company-name" style="color: white;text-transform: uppercase;font-family: Arial, Helvetica, sans-serif;font-size: 1.5rem;font-weight: 500;">User</h3></div></div>`
+  let htmlString= `<html><head><title>PDF REPORT</title></head><body><img src="${dataUri}" alt="Image " style="display: block; width: 100%; height: 100%;object-fit: cover;" /><div style="position: absolute;top: 40%;left: 35%;transform: translate(-50%, -50%);text-align: center;"><h3 id="company-name" style="color: white;text-transform: uppercase;font-family: Arial, Helvetica, sans-serif;font-size: 1.5rem;font-weight: 500;">User</h3></div></div>`
   htmlString+=`<div style="position: relative; padding: 1em; color: #050049; font-family: Arial, Helvetica, sans-serif;"><div style="border-bottom: 3px solid #000; text-align: center;"> <h3 id="company-name" style="margin: 0.5em auto; text-transform: uppercase; letter-spacing: 0.1em; font-weight: 700; font-size: 1.8rem;">${pdfData.companyName}</h3><h5 style="margin: 0 auto; text-transform: uppercase; font-weight: 500; font-size: 1.2rem; letter-spacing: 0.1em;">Thermal Inspection Report</h5><span id="date" style="display: block; margin: 0.5em auto;">Date of Inspection: ${pdfData.date}</span></div><div style="text-align: left;"><h1 style="text-transform: uppercase;margin:1rem 0;">Defect Overview</h1><hr color="#050049" size="2"><table style="width: 100%;"><thead style="color: #050049; font-size: 1.3rem"><th style="padding: 5px;">Sno.</th><th style="padding: 5px;">Array Number</th><th style="padding: 5px;">Number of Defects</th></thead>`
   var ctotal=0;
   for (var i in defects) {
@@ -110,7 +112,7 @@ exports.generatePDF = async (req, res, next) => {
     imgs.push(myimg);
     const dataUri = `data:image/jpeg;base64,${response.data.toString('base64')}`;
     htmlString += `<div style="position: relative; font-family: Arial, Helvetica, sans-serif; padding: 1rem 2rem; width: 30%; margin: 0 auto"><h4 style="display: block; font-size: 1.5rem; color: #050049; text-align: left">DEFECT DETAILS</h4><div style="display: flex; flex-direction: column; align-items: center">`;
-    htmlString += `<img src="${dataUri}" alt="Image ${j}" style="display:block; margin: 10px; width:700px ;margin-bottom: 1rem"/>`;
+    htmlString += `<img src="${dataUri}" alt="Image ${j}" style="display:block; margin: 10px; width:700px;height:500px;margin-bottom: 1rem"/>`;
     htmlString += `<div style="padding: 2rem 3rem; border: 2px solid #050049; border-radius: 30px; width: 150px; text-align: center">`;
     htmlString += `Array No: ${names[j].arrayno}<br />`;
     htmlString += `</div>`;
